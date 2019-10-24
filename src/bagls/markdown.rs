7c4604dbbf45bd2fdd3385c5996a1e444509dbad
Olivier Hériveaux <olivier.heriveaux@ledger.fr>
2019-10-24 17:50:56 +0200

    Work on markdown.rs.
    Added function to retrieve the number of pages.
    Implemented the tests.

--- src/bagls/markdown.rs
+++ src/bagls/markdown.rs
@@ -1,6 +1,7 @@
 
 pub struct Flow<'a> {
-  pub source: &'a str
+  source: &'a str,
+  page_count: u8
 }
 
 pub struct FlowSourceIterator<'a> {
@@ -12,16 +13,34 @@ pub struct FlowSourceIterator<'a> {
 
 const FLOW_MAX_PAGE_COUNT: usize = 16;
 
+/// Calculate the number of page in a given markdown.
+/// * `source` - Markdown code.
+fn compute_page_count(source: &str) -> u8 {
+  let mut result: u8 = 1;
+  for line in source.lines() {
+    if line == "---" {
+      assert!(result < u8::max_value());
+      result += 1;
+    }
+  }
+  result
+}
+
 impl Flow<'_> {
   pub fn new<'a>(source: &'a str) -> Flow<'a> {
     Flow {
-      source: source
+      source: source,
+      page_count: compute_page_count(source)
     }
   }
 
   pub fn lines_in_page<'a>(&'a self, page: u8)
   -> FlowSourceIterator<'a> {
     FlowSourceIterator::new(self.source, page)
+  }
+
+  pub fn page_count(self) -> u8 {
+    self.page_count
   }
 }
 
@@ -79,21 +98,21 @@ impl<'a> Iterator for FlowSourceIterator<'a> {
 mod tests {
   use super::*;
 
+  const test_md: &str = concat!(
+    "page 0 line 0\n",
+    "page 0 line 1\n",
+    "---\n",
+    "page 1 line 0\n",
+    "page 1 line 1\n",
+    "---\n",
+    "page 2 line 0\n",
+    "page 2 line 1\n",
+    "page 2 line 2\n",
+  );
+
   #[test]
   fn test_source_flow_iterator() {
-    let flow = Flow::new(
-      concat!(
-        "page 0 line 0\n",
-        "page 0 line 1\n",
-        "---\n",
-        "page 1 line 0\n",
-        "page 1 line 1\n",
-        "---\n",
-        "page 2 line 0\n",
-        "page 2 line 1\n",
-        "page 2 line 2\n",
-      )
-    );
+    let flow = Flow::new(test_md);
     assert_eq!(
       flow.lines_in_page(0).collect::<Vec<&str>>(),
       ["page 0 line 0", "page 0 line 1"]
@@ -106,6 +125,25 @@ mod tests {
       flow.lines_in_page(2).collect::<Vec<&str>>(),
       ["page 2 line 0", "page 2 line 1", "page 2 line 2"]
     );
+  }
+
+  #[test]
+  fn test_page_count() {
+    assert_eq!(Flow::new(test_md).page_count, 3);
+    assert_eq!(Flow::new("").page_count, 1);
+    assert_eq!(Flow::new("---").page_count, 2);
+    assert_eq!(Flow::new("---\n---").page_count, 3);
+  }
+
+  #[test]
+  #[should_panic]
+  fn test_too_many_pages() {
+    Flow::new(&"---\nhello\n".repeat(255)[..]); // 256 pages
+  }
+
+  #[test]
+  fn test_max_pages() {
+    Flow::new(&"---\nhello\n".repeat(254)[..]); // 255 pages
   }
 }
 
