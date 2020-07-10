dea7e0aa5e18dfd8a809a7bf2e219ec753036327
yhql <victor.servant@ledger.fr>
2020-07-10 14:29:37 +0200

    IO Fix

--- src/c/src.c
+++ src/c/src.c
@@ -1,12 +1,11 @@
 #include "os.h"
 #include "os_io_seproxyhal.h"
 #include "stdint.h"
+#include "string.h"
 
-uint8_t G_io_seproxyhal_spi_buffer[128];
 
-// Yup...
 size_t strlen(const char * str) {
-  char * s = str;
+  const char * s = str;
   unsigned short len = 0;
   while (*s++)
     len++;
@@ -15,23 +14,10 @@ size_t strlen(const char * str) {
 
 unsigned int pic_internal(unsigned int link_address) __attribute__((naked));
 
-#ifdef _GCC_
 unsigned int pic_internal(unsigned int link_address) 
 {
   // compute the delta offset between LinkMemAddr & ExecMemAddr
-  __asm volatile ("mov r2, pc\n");          // r2 = 0x109004
-  __asm volatile ("ldr r1, =pic_internal\n");        // r1 = 0xC0D00001
-  __asm volatile ("add r1, r1, #3\n");     // r1 = 0xC0D00004
-  __asm volatile ("sub r1, r1, r2\n");     // r1 = 0xC0BF7000 (delta between load and exec address)
-
-  // adjust value of the given parameter
-  __asm volatile ("sub r0, r0, r1\n");     // r0 = 0xC0D0C244 => r0 = 0x115244
-  __asm volatile ("bx lr\n");
-}
-#else
-unsigned int pic_internal(unsigned int link_address) 
-{
-  // compute the delta offset between LinkMemAddr & ExecMemAddr
+  __asm volatile (".syntax unified");
   __asm volatile ("mov r2, pc\n");          // r2 = 0x109004
   __asm volatile ("ldr r1, =pic_internal\n");        // r1 = 0xC0D00001
   __asm volatile ("adds r1, r1, #3\n");     // r1 = 0xC0D00004
@@ -41,7 +27,6 @@ unsigned int pic_internal(unsigned int link_address)
   __asm volatile ("subs r0, r0, r1\n");     // r0 = 0xC0D0C244 => r0 = 0x115244
   __asm volatile ("bx lr\n");
 }
-#endif
 
 unsigned char io_event(unsigned char channel) {
     // nothing done with the event, throw an error on the transport layer if
@@ -90,32 +75,22 @@ bolos_ux_params_t G_ux_params;
 extern void ui_rs_idle();
 extern void sample_main();
 
-// __attribute__((section(".boot"))) int main(void) {
 int c_main(void) {
-  // exit critical section
   __asm volatile("cpsie i");
 
-  // ensure exception will work as planned
   os_boot();
   for(;;) {
     UX_INIT();
 
     BEGIN_TRY {
       TRY {
-      
-        //start communication with MCU
         io_seproxyhal_init();
-
         USB_power(0);
         USB_power(1);
-  
-        //set up initial screen
         ui_rs_idle();
-
         sample_main();
       }
       CATCH(EXCEPTION_IO_RESET) {
-                // reset IO and UX
         continue;
       }
       CATCH_ALL {
@@ -126,6 +101,5 @@ int c_main(void) {
     }
     END_TRY;
   }
-//   app_exit();
     return 0;
 }
